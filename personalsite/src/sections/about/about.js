f37d43bf198b71c5f1ae4a703b505fdbc2af0c42
drbarq <J.Tustin@gmail.com>
2020-03-10 16:54:00 -0600

    Adding in back story

--- personalsite/src/sections/about/about.js
+++ personalsite/src/sections/about/about.js
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import styles from './about.module.css';
 
 const about = (
@@ -7,8 +6,8 @@ const about = (
 		<h3 className={styles.h3}>About</h3>
 		<div className={styles.textContainer}>
 			<p className={styles.aboutText}>
-				I am a software developer with a weird story. Scroll down for a long
-				story long
+				I am a software developer with a weird story. Scroll down .... if you
+				dare
 			</p>
 		</div>
 	</div>
--- personalsite/src/sections/backstory/backstory.js
+++ personalsite/src/sections/backstory/backstory.js
@@ -1,14 +1,19 @@
-import React from './node_modules/react';
+import React from 'react';
 import styles from './backstory.module.css';
 
 const backstory = (
 	<div className={styles.backstoryContainer}>
 		<h3 className={styles.h3}>Back Story</h3>
-		<div className={styles.textContainer}>
-			<p className={styles.schoolText}>
-				I am a software developer with a weird story. Scroll down for a long
-				story long
-			</p>
+		<div className={styles.textDiv}>
+			<div className={styles.schoolTextDiv}>
+				<div className={styles.schoolLine1}>
+					<h6>University of Pennsylvania</h6>
+					<p>Sept 2006 - Dec 2010</p>
+				</div>
+				{/* <div className={styles.schoolLine2}>
+					<h8>B.S. Finance</h8>
+				</div> */}
+			</div>
 		</div>
 	</div>
 );
