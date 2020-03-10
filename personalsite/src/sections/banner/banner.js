b707ca5fd9eccbf05ecf605ef1fefa107d0c8951
drbarq <J.Tustin@gmail.com>
2020-03-10 15:20:31 -0600

    working on new design

--- personalsite/src/sections/banner/banner.js
+++ personalsite/src/sections/banner/banner.js
@@ -3,32 +3,11 @@ import joeBanner from '../../images/joeBanner.jpg';
 
 import styles from './banner.module.css';
 
-// function banner() {
-// 	return (
-// 		// <section>
-// 		<h1>Section One</h1>
-// 		// </section>
-// 	);
-// }
-
 const banner = (
-	// <React.Fragment>
 	<div className={styles.bannerContainer}>
 		<h1 className={styles.h1}>Joe Tustin</h1>
 		<img className={styles.bannerImg} src={joeBanner} />
 	</div>
-	// {/* </React.Fragment> */}
 );
-
-// class Banner extends React.Component {
-// 	render() {
-// 		return (
-// 			<section>
-// 				<h1>Section One</h1>
-// 				<img src={joeBanner} />
-// 			</section>
-// 		);
-// 	}
-// }
 
 export default banner;
