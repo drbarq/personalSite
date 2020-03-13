687cfaac99f5e534f2207dbfe1f7650057e09534
drbarq <J.Tustin@gmail.com>
2020-03-13 17:36:12 -0600

    added in csv with points render on map

--- personalsite/src/sections/map/gMapContainer.js
+++ personalsite/src/sections/map/gMapContainer.js
@@ -1,41 +1,40 @@
 import React, { Component } from 'react';
 import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
 import styles from './map.module.css';
+
+import gMapData from '../../Data/gMapData';
+
 const config = {
 	gMapsApi: process.env.REACT_APP_GOOGLE_MAP_API
 };
 
-const mapStyles = {
-	// display: 'flex',
-	// flexDirection: 'column',
-	width: '50%',
-	height: '50%'
-};
-
 export class MapContainer extends Component {
-	// displayMarkers = () => {
-	// 	return this.state.stores.map((store, index) => {
-	// 		return (
-	// 			<Marker
-	// 				key={index}
-	// 				id={index}
-	// 				position={{
-	// 					lat: store.latitude,
-	// 					lng: store.longitude
-	// 				}}
-	// 				onClick={() => console.log('You clicked me!')}
-	// 			/>
-	// 		);
-	// 	});
-	// };
+	displayMarkers = () => {
+		return gMapData.map((point, index) => {
+			return (
+				<Marker
+					key={index}
+					id={index}
+					position={{
+						lat: point.LATITUDE,
+						lng: point.LONGITUDE
+					}}
+					onClick={() => console.log(`what else is here: ${point}`)}
+				/>
+			);
+		});
+	};
 
 	render() {
+		// console.log(gMapData[0].LATITUDE);
 		return (
 			<Map
 				google={this.props.google}
 				zoom={2}
 				initialCenter={{ lat: 0, lng: 0 }}
-			/>
+			>
+				{this.displayMarkers()}
+			</Map>
 		);
 	}
 }
