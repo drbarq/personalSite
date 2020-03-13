c1a349cb7ba6471a348aedde92dbb658cef074d6
drbarq <J.Tustin@gmail.com>
2020-03-13 14:36:01 -0600

    added in map container

--- personalsite/src/sections/map/map.js
+++ personalsite/src/sections/map/map.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import styles from './map.module.css';
+import SectionHeader from '../components/sectionHeader./sectionHeader';
 
 const config = {
 	gMapsApi: process.env.REACT_APP_GOOGLE_MAP_API
 };
 
 function map() {
-	return <div>MAP</div>;
+	return (
+		<div className={styles.mapContainer}>
+			<SectionHeader
+				titleOne={`Where in the world was Joe SanDiego`}
+				titleTwo={'Ayyyyy Lit ass maps'}
+			/>
+			<div className={styles.gMapContainer}>MAP</div>
+		</div>
+	);
 }
 
 export default map;
