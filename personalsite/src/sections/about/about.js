520e4cdb8db8c0e07918f14b441aa2e8d46e0460
drbarq <J.Tustin@gmail.com>
2020-03-14 11:41:48 -0600

    adding in padding

--- personalsite/src/sections/about/about.js
+++ personalsite/src/sections/about/about.js
@@ -33,26 +33,3 @@ function about() {
 }
 
 export default about;
-// = (
-// 	<div className={styles.aboutContainer}>
-// 		<h3 className={styles.h3}>About</h3>
-// 		<div className={styles.textContainer}>
-// 			<p className={styles.aboutText}>
-// 				I am a software developer with a weird story. Scroll down .... if you
-// 				dare
-// 			</p>
-// 		</div>
-// 	</div>
-// );
-
-// const about = (
-// 	<div className={styles.aboutContainer}>
-// 		<h3 className={styles.h3}>About</h3>
-// 		<div className={styles.textContainer}>
-// 			<p className={styles.aboutText}>
-// 				I am a software developer with a weird story. Scroll down .... if you
-// 				dare
-// 			</p>
-// 		</div>
-// 	</div>
-// );
