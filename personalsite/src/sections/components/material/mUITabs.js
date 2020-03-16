a4d200b2a951acbce7ca53d0b8e871c72a01687b
drbarq <J.Tustin@gmail.com>
2020-03-16 12:09:01 -0600

    addin gin tabpanel styles

--- personalsite/src/sections/components/material/mUITabs.js
+++ personalsite/src/sections/components/material/mUITabs.js
@@ -22,7 +22,7 @@ function TabPanel(props) {
 
 	return (
 		<Typography
-			// className={styles.tabPanel}
+			className={styles.tabPanel}
 			component="div"
 			role="tabpanel"
 			hidden={value !== index}
