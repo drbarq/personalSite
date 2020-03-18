98684b7bfc9fa873c32144ae9f9391c5cf113771
drbarq <J.Tustin@gmail.com>
2020-03-18 15:57:36 -0600

    adding in experience container

--- personalsite/src/sections/experience/experience.js
+++ personalsite/src/sections/experience/experience.js
@@ -5,8 +5,9 @@ import styles from './experience.module.css';
 import SectionHeader from '../../components/sectionHeader/sectionHeader';
 
 import ExpansionPanelATT from './expansionPanel/expansionPanelATT';
+import ExpansionPanelSG from './expansionPanel/expansionPanelSG';
 
-const experienceContainer = (
+const experienceContainerATT = (
 	<div className={styles.companyContainer}>
 		<div className={styles.companyNameContainer}>
 			<div className={styles.companyName}>AT&T</div>
@@ -18,30 +19,28 @@ const experienceContainer = (
 	</div>
 );
 
+const experienceContainerSG = (
+	<div className={styles.companyContainer}>
+		<div className={styles.companyNameContainer}>
+			<div className={styles.companyName}>SendGrid</div>
+		</div>
+
+		<div className={styles.additionalInfoContainer}>
+			<ExpansionPanelSG />
+		</div>
+	</div>
+);
+
 export default function Experience() {
 	return (
 		<div className={styles.sectionContainer}>
 			<SectionHeader titleOne="always be closing" titleTwo="Experience" />
 			<div className={styles.contentContainer}>
-				<div className={styles.experienceContainer}>{experienceContainer}</div>
-
-				{/* <ExpansionPanel />s */}
+				<div className={styles.experienceContainer}>
+					{experienceContainerSG}
+					{experienceContainerATT}
+				</div>
 			</div>
 		</div>
 	);
-}
-
-{
-	/* <div className={styles.companyNameContainer}>
-			<div className={styles.companyName}>SendGrid</div>
-		</div>
-
-		<div className={styles.lineOne}>
-			<div className={styles.experienceSubline}>Sr. Account Executive</div>
-			<div className={styles.educationDate}>Feb 2016 - Oct 2017</div>
-		</div>
-		<div className={styles.lineTwo}>
-			<div className={styles.degree}>Put Expansion panel here</div>
-
-		</div> */
 }
