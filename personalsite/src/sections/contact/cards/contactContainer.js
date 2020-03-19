afc05434824ff4117b51940ea711d5a59303655a
drbarq <J.Tustin@gmail.com>
2020-03-19 14:40:59 -0600

    building contact container

--- personalsite/src/sections/contact/cards/contactContainer.js
+++ personalsite/src/sections/contact/cards/contactContainer.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import styles from '../contact.module.css';
+import LinkComponent from './linkComponent';
+
+import { faAt } from '@fortawesome/free-solid-svg-icons';
 
 export default function ContactContainer() {
 	return (
 		<div className={styles.contactContainer}>
 			<div className={styles.contactTitleContainer}>
-				<div className={styles.status}>📨 Contact Me 📞</div>
+				<div className={styles.title}>📨 Contact Me 📞</div>
 			</div>
-			<div className={styles.additionalInfoContainer}>
+			<div className={styles.contactLinksContainer}>
+				<LinkComponent
+					link="mailto:J.Tustin@gmail.com?subject=Job Offer: $500K Salary"
+					title="Email Me"
+					icon={faAt}
+				/>
 				<div className={styles.statement}></div>
 			</div>
 		</div>
---
+++ personalsite/src/sections/contact/cards/linkComponent.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import styles from '../contact.module.css';
+
+import { faAt } from '@fortawesome/free-solid-svg-icons';
+import Typography from '@material-ui/core/Typography';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+export default function LinkComponent({ link, title, icon }) {
+	return (
+		<a
+			// href="mailto:J.Tustin@gmail.com?subject=Job Offer: $500K Salary"
+			href={link}
+			target="_blank"
+		>
+			<div className={styles.linkLabel}>
+				<FontAwesomeIcon icon={icon} className={styles.icons} />
+				<Typography className={styles.link} variant="body2" component="p">
+					{title}
+				</Typography>
+			</div>
+		</a>
+	);
+}
