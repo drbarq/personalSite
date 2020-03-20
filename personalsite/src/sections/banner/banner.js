73942ae6f55fcf5a85ffe7860f10eb8101c6ce36
drbarq <J.Tustin@gmail.com>
2020-03-20 14:03:44 -0600

    building in banner behavior

--- personalsite/src/sections/banner/banner.js
+++ personalsite/src/sections/banner/banner.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import joeBanner from '../../images/joeBanner.jpg';
+import joeBanner from '../../images/joeBanner.JPG';
 import Breadcrumbs from '@material-ui/core/Breadcrumbs';
 
 import Breadcrumb from './breadcrumbs/breadcrumb';
