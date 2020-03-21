9a76f11540832dab34fbbb23e2ef6a6ef2e15f53
drbarq <J.Tustin@gmail.com>
2020-03-21 17:35:53 -0600

    fixing spacing for banner image

--- personalsite/src/sections/banner/banner.js
+++ personalsite/src/sections/banner/banner.js
@@ -2,6 +2,8 @@ import React from 'react';
 import joeBanner from '../../images/joeBanner.jpg';
 import Breadcrumbs from '@material-ui/core/Breadcrumbs';
 
+import JoeImageBig from '../../images/DSC08263.JPG';
+
 import Breadcrumb from './breadcrumbs/breadcrumb';
 
 import styles from './banner.module.css';
@@ -11,7 +13,8 @@ function Banner() {
 		<div id="banner" className={styles.sectionContainer}>
 			<div className={styles.bannerContainer}>
 				<div className={styles.imgContainer}>
-					<img className={styles.bannerImg} src={joeBanner} />
+					{/* <img className={styles.bannerImg} src={joeBanner} /> */}
+					<img className={styles.bannerImg} src={JoeImageBig} />
 				</div>
 				<div className={styles.nameContainer}>
 					<div className={styles.name}>Joe</div>
