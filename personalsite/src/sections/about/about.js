9752aab9c252cc9d5229f7c48dbb3a09afa56d58
drbarq <J.Tustin@gmail.com>
2020-03-21 13:56:44 -0600

    removed old code

--- personalsite/src/sections/about/about.js
+++ personalsite/src/sections/about/about.js
@@ -3,8 +3,6 @@ import styles from './about.module.css';
 
 import SectionHeader from '../../components/sectionHeader/sectionHeader';
 
-import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
-
 function about() {
 	return (
 		<div id="about" className={styles.sectionContainer}>
@@ -33,16 +31,3 @@ function about() {
 }
 
 export default about;
-
-{
-	/* <p>
-Below you will find the timeline of events leading me to this point.
-</p>
-{/* <p style={{ paddingBottom: '1rem' }}> */
-}
-{
-	/* <p>
-Highlights include My Sales Career, 1.5 year motorcycle trip around
-the world, and starting a business
-</p>  */
-}
