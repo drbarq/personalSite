063b5c9fdf2407383180e88f7179ccfe80039ddd
drbarq <J.Tustin@gmail.com>
2020-03-25 10:55:00 -0600

    removed backstory

--- personalsite/src/App.js
+++ personalsite/src/App.js
@@ -3,7 +3,6 @@ import styles from './App.module.css';
 
 import Banner from './sections/banner/banner';
 import About from './sections/about/about';
-import Backstory from './sections/backstory/backstory';
 import Map from './sections/map/map';
 import Projects from './sections/projects/projectSection';
 import Bredkrum from './sections/bredkrum/bredkrum';
