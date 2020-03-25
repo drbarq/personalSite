9affa2d650b5a33407d196d82cd316dda398ad44
drbarq <J.Tustin@gmail.com>
2020-03-25 12:24:15 -0600

    removed link security issue

--- personalsite/src/sections/contact/cards/linkComponent.js
+++ personalsite/src/sections/contact/cards/linkComponent.js
@@ -5,7 +5,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export default function LinkComponent({ link, title, icon }) {
 	return (
-		<a href={link} target="_blank">
+		<a href={link} target="_blank" rel="noopener noreferrer">
 			<div className={styles.linkLabel}>
 				<FontAwesomeIcon icon={icon} className={styles.icons} />
 				<Typography className={styles.link} variant="body2" component="p">
