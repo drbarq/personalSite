da662896db8bee0973da4c674040e412bb544ebe
drbarq <J.Tustin@gmail.com>
2020-03-25 12:17:25 -0600

    added alt prop for banner

--- personalsite/src/sections/banner/banner.js
+++ personalsite/src/sections/banner/banner.js
@@ -1,8 +1,5 @@
 import React from 'react';
-
 import bannerCropped from '../../images/bannerResizedCrop.png';
-import bannerResized from '../../images/bannerResized.JPG';
-
 import Breadcrumb from './breadcrumbs/breadcrumb';
 import styles from './banner.module.css';
 
@@ -11,7 +8,11 @@ function Banner() {
 		<div id="banner" className={styles.sectionContainer}>
 			<div className={styles.bannerContainer}>
 				<div className={styles.imgContainer}>
-					<img className={styles.bannerImg} src={bannerCropped} />
+					<img
+						className={styles.bannerImg}
+						alt="photo of Joe at Torres del Paine, Chile"
+						src={bannerCropped}
+					/>
 				</div>
 				<div className={styles.nameContainer}>
 					<div className={styles.name}>Joe</div>
